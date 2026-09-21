abcb4e047177aa7d766f0b7dde782684bb4a89eb
Gustavo Teixeira <teixeiragoesgustavo@gmail.com>
2026-09-21 11:50:00 +0000

    update: will remove the database dir

--- src/database/index.js
+++
@@ -1,8 +0,0 @@
-import sqlite3 from 'sqlite3'
-import path from 'path'
-
-const mySqlite = sqlite3.verbose()
-
-const db = new mySqlite.Database(path.join(__dirname, '/registered.db'))
-
-db.run('CREATE TABLE IF NOT EXISTS registered()')
