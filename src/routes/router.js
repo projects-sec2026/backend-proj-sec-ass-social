5441dc68035c6f7687bf055f7369713311ef967c
Gustavo Teixeira <teixeiragoesgustavo@gmail.com>
2026-09-22 12:54:34 +0000

    add: add the return the json of root post router

--- src/routes/router.js
+++ src/routes/router.js
@@ -25,6 +25,12 @@ router.post('/', async (req, res) => {
         .from('atendimentos')
         .insert([registration])
 
+    if(!error) {
+        return res.json(true)
+    } else {
+        return res.json(error)
+    }
+
 })
 
 router.post('/registered', verifyPassword, async (req, res) => {
@@ -36,7 +42,7 @@ router.post('/registered', verifyPassword, async (req, res) => {
     if(error) {
         return res.status(400).json(error)
     } else {
-        return res.status(200).json({data: data, loged: true})
+        return res.status(200).json({data: data})
     }
 
 })
